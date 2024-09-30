706099f511686468c5538054cfb63e79b816ef73
Katielly Reis <77698539+Katielly@users.noreply.github.com>
2024-09-29 22:26:31 -0400

    feat: add campos date e time

--- app/src/main/java/com/dev_mobile/list_task/CreateTask.kt
+++ app/src/main/java/com/dev_mobile/list_task/CreateTask.kt
@@ -1,123 +1,149 @@
 package com.dev_mobile.list_task
 
-import androidx.compose.foundation.background
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dev_mobile.list_task.ui.theme.ListtaskTheme
-import java.util.Date
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
+@SuppressLint("DefaultLocale")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateTask(
     backScreen: () -> Unit
 ) {
+    var taskDescription by remember { mutableStateOf("") }
 
-    var openSelectDate by remember {
-        mutableStateOf(false)
-    }
+    val currentTime = Calendar.getInstance()
 
-    val datePickerState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+    var timeText by remember { mutableStateOf("") }
 
-    val selectedDate = datePickerState.selectedDateMillis?.let {
-        convertMillisToDate(it)
-    } ?: ""
+    val timePickerDialog = TimePickerDialog(
+        LocalContext.current, { _, hourOfDay, minute ->
+            timeText = String.format(
+                "%02d:%02d %s",
+                if (hourOfDay % 12 == 0) 12 else hourOfDay % 12,
+                minute,
+                if (hourOfDay >= 12) "PM" else "AM"
+            )
+        }, currentTime.get(Calendar.HOUR_OF_DAY), currentTime.get(Calendar.MINUTE), false
+    )
 
+    var dateText by remember { mutableStateOf("") }
 
-//    var selectedDate by remember {
-//        mutableStateOf<Long?>(null)
-//    }
-    //TODO CONTROLE DENTRO POP-UP DATE
-//    if (selectedDate != null) {
-//        val date = Date(selectedDate!!)
-//    }
+    val datePickerDialog = DatePickerDialog(
+        LocalContext.current,
+        { _, year, month, dayOfMonth ->
+            val date = Calendar.getInstance()
+            date.set(year, month, dayOfMonth)
+            dateText = SimpleDateFormat("E, MMM d, yyyy", Locale.getDefault()).format(date.time)
+        },
+        currentTime.get(Calendar.YEAR),
+        currentTime.get(Calendar.MONTH),
+        currentTime.get(Calendar.DAY_OF_MONTH)
+    )
 
-
-    if (openSelectDate) {
-        DatePickerDialog(onDismissRequest = {
-            //TODO FECHA DATA
-        }, confirmButton = {
-            //TODO CONFIRMA DATA
-        }, dismissButton = {
-
-        }) {
-            DatePicker(state = datePickerState)
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Add Nova Tarefa") })
         }
-    }
-
-    Scaffold(topBar = {
-        TopAppBar(title = { Text("Add Nova Tarefa") })
-    }) {
+    ) {
         Column(
             modifier = Modifier
                 .padding(it)
-                .fillMaxSize()
-                .background(Color.Red)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            //TODO CAMPO TEXTO AREA
-            // SALVAR NOTA
-            // HORAS
-            // DATA
-            // BOTÃO
 
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .background(Color.Green)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top
             ) {
-//                OutlinedTextField(
-//                    value = selectedDate,
-//                    onValueChange = { },
-//                    label = { Text("DOB") },
-//                    readOnly = true,
-//                    trailingIcon = {
-//                        IconButton(onClick = { openSelectDate = !openSelectDate }) {
-//                            Icon(
-//                                imageVector = Icons.Default.DateRange,
-//                                contentDescription = "Select date"
-//                            )
-//                        }
-//                    },
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .height(64.dp)
-//                )
+
+                TextField(
+                    value = taskDescription,
+                    onValueChange = { taskDescription = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(152.dp),
+                    label = { Text("Tarefa") },
+                )
+
+                //TODO add nota
+
+                OutlinedTextField(
+                    value = timeText,
+                    onValueChange = {},
+                    label = { Text("Horario") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    readOnly = true,
+                    trailingIcon = {
+                        IconButton(onClick = { timePickerDialog.show() }) {
+                            Icon(
+                                imageVector = Icons.Default.AccessTime,
+                                contentDescription = "Select Time"
+                            )
+                        }
+                    }
+                )
+
+                OutlinedTextField(
+                    value = dateText,
+                    onValueChange = {},
+                    label = { Text("Data") },
+                    modifier = Modifier.fillMaxWidth(),
+                    readOnly = true,
+                    trailingIcon = {
+                        IconButton(onClick = { datePickerDialog.show() }) {
+                            Icon(
+                                imageVector = Icons.Default.DateRange,
+                                contentDescription = "Select Date"
+                            )
+                        }
+                    }
+                )
             }
 
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.Blue),
-                horizontalArrangement = Arrangement.Center
+                    .height(50.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Bottom
             ) {
                 Button(onClick = { backScreen() }) {
                     Text("Salvar")
@@ -125,10 +151,6 @@ fun CreateTask(
             }
         }
     }
-}
-
-fun convertMillisToDate(it: Long) {
-
 }
 
 @Preview
